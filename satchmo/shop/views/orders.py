8cb51cbc6d2e447c0c1d806631d9a258c5588435
Jude Boden <judeboden@hotmail.com>
2009-11-20 12:25:55 +0000

    Don't show orders where status is blank - hides orders from order history if they have not yet completed

--- satchmo/shop/views/orders.py
+++ satchmo/shop/views/orders.py
@@ -11,7 +11,7 @@ def order_history(request):
     orders = None
     try:
         contact = Contact.objects.from_request(request, create=False)
-        orders = Order.objects.filter(contact=contact).order_by('-time_stamp')
+        orders = Order.objects.filter(contact=contact).exclude(status="").order_by('-time_stamp')#Work around for order creation problem where new orders show in order_history page before they've been completed
     
     except Contact.DoesNotExist:
         contact = None
