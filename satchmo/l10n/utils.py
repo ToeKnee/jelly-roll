8b4cc8f0854d24d518115bb7a0c4d1267866ab04
Anthony Batchelor <tony@ynottony.net>
2011-05-10 19:55:18 +0100

    Make sure we return a unicode string for money_format

--- satchmo/l10n/utils.py
+++ satchmo/l10n/utils.py
@@ -7,6 +7,6 @@ log = logging.getLogger('l10n.utils')
 
 def money_format(value, grouping=True):
     """
-    Convert Decimal to a money formatted string.
+    Convert Decimal to a money formatted unicode string.
     """
-    return locale.currency(value, grouping=grouping)
+    return locale.currency(value, grouping=grouping).decode("utf-8")
