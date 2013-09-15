a9ba74c55ee900d9708aea01c0173adac0a2b0a3
Anthony Batchelor <tony@ynottony.net>
2013-09-15 22:30:06 +0100

    Royal Mail contract shipping module

---
+++ satchmo/shipping/modules/royalmailcontract/config.py
@@ -0,0 +1,54 @@
+from django.utils.translation import ugettext_lazy as _
+
+from satchmo.configuration import config_get, config_get_group, \
+    config_register_list, DecimalValue, MultipleStringValue
+from satchmo.l10n.models import Country
+
+SHIP_MODULES = config_get('SHIPPING', 'MODULES')
+SHIP_MODULES.add_choice(('satchmo.shipping.modules.royalmailcontract', _('Royal Mail Contract')))
+
+SHIPPING_GROUP = config_get_group('SHIPPING')
+
+config_register_list(
+    DecimalValue(SHIPPING_GROUP,
+                 'PACKING_FEE',
+                 description=_("[Royal Mail Contract] Packing Fee"),
+                 requires=SHIP_MODULES,
+                 requiresvalue='satchmo.shipping.modules.royalmailcontract',
+                 default="0.30"),
+
+    DecimalValue(SHIPPING_GROUP,
+                 'PER_RATE_EU',
+                 description=_("[Royal Mail Contract] Per item price E.U."),
+                 requires=SHIP_MODULES,
+                 requiresvalue='satchmo.shipping.modules.royalmailcontract',
+                 default="1.09"),
+
+    DecimalValue(SHIPPING_GROUP,
+                 'PER_KG_EU',
+                 description=_("[Royal Mail Contract] Per Kg price E.U."),
+                 requires=SHIP_MODULES,
+                 requiresvalue='satchmo.shipping.modules.royalmailcontract',
+                 default="4.33"),
+
+    DecimalValue(SHIPPING_GROUP,
+                 'PER_RATE_ROW',
+                 description=_("[Royal Mail Contract] Per item price R.o.W."),
+                 requires=SHIP_MODULES,
+                 requiresvalue='satchmo.shipping.modules.royalmailcontract',
+                 default="1.14"),
+
+    DecimalValue(SHIPPING_GROUP,
+                 'PER_KG_ROW',
+                 description=_("[Royal Mail Contract] Per Kg price R.o.W."),
+                 requires=SHIP_MODULES,
+                 requiresvalue='satchmo.shipping.modules.royalmailcontract',
+                 default="6.65"),
+
+    MultipleStringValue(SHIPPING_GROUP,
+                        'EXCLUDE_COUNTRY',
+                        description=_("Countries excluded from Royal Mail Contract shipping."),
+                        help_text=_("Select the countries that you want to exclude."),
+                        default=[],
+                        choices=[(c.iso2_code, c.printable_name) for c in Country.objects.exclude(iso2_code="GB")])
+)
---
+++ satchmo/shipping/modules/royalmailcontract/shipper.py
@@ -0,0 +1,95 @@
+"""
+Each shipping option uses the data in an Order object to calculate the
+shipping cost and return the value
+"""
+import math
+from decimal import Decimal, ROUND_UP
+from django.utils.translation import ugettext_lazy as _
+from satchmo.configuration import config_value
+from satchmo.shipping.modules.base import BaseShipper
+
+
+class Shipper(BaseShipper):
+    id = "RoyalMailContract"
+
+    def description(self):
+        """
+        A basic description that will be displayed to the user when selecting their shipping options
+        """
+        return _("Airmail (Tracking not available)")
+
+    def cost(self):
+        """
+        Complex calculations can be done here as long as the return value is a dollar figure
+        """
+        fee = Decimal("0.00")
+        weight = Decimal("0.00")
+
+        packing_fee = config_value('SHIPPING', 'PACKING_FEE')
+        if self.shipping_to_eu():
+            per_item_rate = config_value('SHIPPING', 'PER_RATE_EU')
+            per_kg_rate = config_value('SHIPPING', 'PER_KG_EU')
+        else:
+            per_item_rate = config_value('SHIPPING', 'PER_RATE_ROW')
+            per_kg_rate = config_value('SHIPPING', 'PER_KG_ROW')
+
+        fee += per_item_rate  # Royal Mail calls a package an item,
+                              # this will need revised if we ever do
+                              # auto splitting of orders into multiple
+                              # packages
+
+        for cartitem in self.cart.cartitem_set.all():
+            if cartitem.product.is_shippable:
+                weight += cartitem.product.weight * cartitem.quantity
+
+        # Round weight up to nearest 100g
+        weight = weight * 10  # Kgs to tens of grams
+        weight = math.ceil(weight)  # Round to nearest 100g
+        weight = weight / 10  # Tens of grams to Kgs
+        fee += Decimal(str(weight)) * per_kg_rate
+
+        # Add TAX
+        if self.shipping_to_eu():
+            fee = fee * Decimal(str(1.2))  # 20% VAT - Not happy about
+                                           #this, but this is the only
+                                           #module that is charging
+                                           #VAT atm :(
+
+        # Add packing fee
+        fee += packing_fee
+
+        # Round to nearest penny
+        fee = fee.quantize(Decimal('.01'), rounding=ROUND_UP)
+
+        return fee
+
+    def method(self):
+        """
+        Describes the actual delivery service (Mail, FedEx, DHL, UPS, etc)
+        """
+        return _("Royal Mail")
+
+    def expectedDelivery(self):
+        """
+        Can be a plain string or complex calcuation returning an actual date
+        """
+        if self.shipping_to_eu():
+            days = _("3 - 7 working days")
+        else:
+            days = _("5 - 10 working days")
+        return days
+
+    def valid(self, order=None):
+        """
+        Exclude countries that we don't want to ship to, also exclude
+        the United Kingdom as this shipping module doesn't support GB.
+        """
+        excluded_countries = config_value('SHIPPING', 'EXCLUDE_COUNTRY')
+        excluded_countries.append(u'GB')
+        return self.contact.shipping_address.country.iso2_code not in excluded_countries
+
+    def shipping_to_eu(self):
+        """
+        If the contact's shipping address is in an EU country
+        """
+        return self.contact.shipping_address.country.eu
