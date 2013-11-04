3a11b84bcd5f081a15ac79effadffef3d12fe721
Anthony Batchelor <tony@ynottony.net>
2013-11-04 20:58:14 +0000

    Improve configuration for Royal Mail Contract shipping module

--- satchmo/shipping/modules/royalmailcontract/config.py
+++ satchmo/shipping/modules/royalmailcontract/config.py
@@ -1,25 +1,33 @@
 from django.utils.translation import ugettext_lazy as _
-
-from satchmo.configuration import config_get, config_get_group, \
-    config_register_list, DecimalValue, MultipleStringValue
+from satchmo.configuration import (
+    config_get,
+    config_register_list,
+    ConfigurationGroup,
+    DecimalValue,
+    MultipleStringValue
+)
 from satchmo.l10n.models import Country
 
 SHIP_MODULES = config_get('SHIPPING', 'MODULES')
 SHIP_MODULES.add_choice(('satchmo.shipping.modules.royalmailcontract', _('Royal Mail Contract')))
 
-SHIPPING_GROUP = config_get_group('SHIPPING')
+SHIPPING_GROUP = ConfigurationGroup('satchmo.shipping.modules.royalmailcontract',
+                                    _('Royal Mail Contract Shipping Settings'),
+                                    requires=SHIP_MODULES,
+                                    ordering=101)
+
 
 config_register_list(
     DecimalValue(SHIPPING_GROUP,
                  'PACKING_FEE',
-                 description=_("[Royal Mail Contract] Packing Fee"),
+                 description=_("Packing Fee"),
                  requires=SHIP_MODULES,
                  requiresvalue='satchmo.shipping.modules.royalmailcontract',
                  default="0.30"),
 
     DecimalValue(SHIPPING_GROUP,
                  'MAX_WEIGHT_PER_ITEM',
-                 description=_("[Royal Mail Contract] Max weight per item in Kgs (packet)"),
+                 description=_("Max weight per item in Kgs (packet)"),
                  help_text=_("The orders weight is rounded up and divided by this and multiplied by the per item price"),
                  requires=SHIP_MODULES,
                  requiresvalue='satchmo.shipping.modules.royalmailcontract',
@@ -27,28 +35,28 @@ config_register_list(
 
     DecimalValue(SHIPPING_GROUP,
                  'PER_RATE_EU',
-                 description=_("[Royal Mail Contract] Per item price E.U."),
+                 description=_("Per item price E.U."),
                  requires=SHIP_MODULES,
                  requiresvalue='satchmo.shipping.modules.royalmailcontract',
                  default="1.09"),
 
     DecimalValue(SHIPPING_GROUP,
                  'PER_KG_EU',
-                 description=_("[Royal Mail Contract] Per Kg price E.U."),
+                 description=_("Per Kg price E.U."),
                  requires=SHIP_MODULES,
                  requiresvalue='satchmo.shipping.modules.royalmailcontract',
                  default="4.33"),
 
     DecimalValue(SHIPPING_GROUP,
                  'PER_RATE_ROW',
-                 description=_("[Royal Mail Contract] Per item price R.o.W."),
+                 description=_("Per item price R.o.W."),
                  requires=SHIP_MODULES,
                  requiresvalue='satchmo.shipping.modules.royalmailcontract',
                  default="1.14"),
 
     DecimalValue(SHIPPING_GROUP,
                  'PER_KG_ROW',
-                 description=_("[Royal Mail Contract] Per Kg price R.o.W."),
+                 description=_("Per Kg price R.o.W."),
                  requires=SHIP_MODULES,
                  requiresvalue='satchmo.shipping.modules.royalmailcontract',
                  default="6.65"),
