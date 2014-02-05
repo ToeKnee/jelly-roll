84bb54746c31cef4986f298c71ad8391a1ae7683
Anthony Batchelor <tony@ynottony.net>
2014-02-05 13:39:47 +0000

    Add EU to countries list in admin

--- satchmo/l10n/admin.py
+++ satchmo/l10n/admin.py
@@ -1,17 +1,16 @@
 from satchmo.l10n.models import Country, AdminArea
 from django.contrib import admin
-from django.utils.translation import get_language, ugettext_lazy as _
 
 
 class AdminArea_Inline(admin.TabularInline):
     model = AdminArea
     extra = 1
 
+
 class CountryOptions(admin.ModelAdmin):
-    list_display = ('printable_name', 'iso2_code',)
-    list_filter = ('continent', 'active')
+    list_display = ('printable_name', 'iso2_code', 'eu',)
+    list_filter = ('continent', 'active', 'eu',)
     search_fields = ('name', 'iso2_code', 'iso3_code')
     inlines = [AdminArea_Inline]
 
 admin.site.register(Country, CountryOptions)
-
