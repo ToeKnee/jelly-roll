b8f6f1ad8807093c64129ce5d83280170d2f62db
Anthony Batchelor <tony@ynottony.net>
2014-03-17 21:25:37 +0000

    Remove overriding logging set up

--- satchmo/__init__.py
+++ satchmo/__init__.py
@@ -1,7 +1,6 @@
 VERSION = (0, 5, 0)
-import logging
 import locale
-import os
+
 
 def get_version():
     "Returns the version as a human-format string."
@@ -13,24 +12,3 @@ def get_version():
     return v
 
 locale.setlocale(locale.LC_ALL, '')
-
-#Configure logging
-LOGFILE = os.path.join("/", "var", "log", "jelly-roll.log")
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-                    datefmt='%a, %d %b %Y %H:%M:%S',
-                    filename=LOGFILE,
-                    filemode='a+')
-
-# define a Handler which writes INFO messages or higher to the sys.stderr
-fileLog = logging.FileHandler(LOGFILE, 'a+')
-fileLog.setLevel(logging.DEBUG)
-# set a format which is simpler for console use
-formatter = logging.Formatter('%(asctime)s %(name)-12s: %(levelname)-8s %(message)s')
-# tell the handler to use this format
-fileLog.setFormatter(formatter)
-# add the handler to the root logger
-logging.getLogger('').addHandler(fileLog)
-logging.getLogger('caching').setLevel(logging.INFO)
-logging.info("Jelly Roll started")
-
