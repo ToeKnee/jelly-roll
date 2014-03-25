a7a4f9db9b1e73e602610c388fb4be1451e5be82
Anthony Batchelor <tony@ynottony.net>
2014-03-25 22:34:35 +0000

    Handle None values in money_format

--- satchmo/l10n/utils.py
+++ satchmo/l10n/utils.py
@@ -1,10 +1,14 @@
 import locale
 import logging
+from decimal import Decimal
 
-log = logging.getLogger('l10n.utils')
+log = logging.getLogger(__name__)
+
 
 def money_format(value, grouping=True):
     """
     Convert Decimal to a money formatted unicode string.
     """
+    if value is None:
+        value = Decimal("0.00")
     return locale.currency(value, grouping=grouping).decode("utf-8")
