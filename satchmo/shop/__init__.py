e2a15792526268fd6f2745111a32a91db8a087f4
Anthony Batchelor <tony@ynottony.net>
2015-02-01 22:38:33 +0000

    Simplify init - remove threaded_multihost

--- satchmo/shop/__init__.py
+++ satchmo/shop/__init__.py
@@ -1,12 +1,7 @@
-from satchmo_settings import get_satchmo_setting
 from satchmo.shop import signals
-from satchmo.shop.exceptions import *
 from satchmo.shop.listeners import veto_out_of_stock
+
 import logging
 log = logging.getLogger(__name__)
-
-if get_satchmo_setting('MULTISHOP'):
-    log.debug('patching for multishop')
-    from threaded_multihost import multihost_patch
 
 signals.satchmo_cart_add_verify.connect(veto_out_of_stock)
