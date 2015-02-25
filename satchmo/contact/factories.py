9fc1bf2363d818b0372b8bb12452e9a9c7bbdb74
Anthony Batchelor <tony@ynottony.net>
2015-02-25 21:11:50 +0000

    Improve unicode support in contact factory

--- satchmo/contact/factories.py
+++ satchmo/contact/factories.py
@@ -9,10 +9,10 @@ class AddressBookFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = AddressBook
 
-    addressee = factory.LazyAttribute(lambda a: '{0} {1}'.format(a.contact.first_name, a.contact.last_name))
-    street1 = "123 EZ Street"
-    city = "The big easy"
-    postal_code = "KY12 8OR"
+    addressee = factory.LazyAttribute(lambda a: u'{0} {1}'.format(a.contact.first_name, a.contact.last_name))
+    street1 = u"123 EZ Street"
+    city = u"The big easy"
+    postal_code = u"KY12 8OR"
     country = factory.SubFactory(CountryFactory)
     is_default_shipping = True
     is_default_billing = True
@@ -22,8 +22,8 @@ class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
         model = User
 
-    first_name = 'Joe'
-    last_name = 'Bloggs'
+    first_name = u'Joe'
+    last_name = u'Bloggs'
     username = factory.LazyAttributeSequence(lambda a, n: '{0}-{1}-{2}'.format(a.first_name, a.last_name, n).lower())
     email = factory.LazyAttributeSequence(lambda a, n: '{0}+{1}@example.com'.format(a.username, n).lower())
     is_active = True
@@ -45,7 +45,7 @@ class ContactFactory(factory.django.DjangoModelFactory):
 
     first_name = factory.LazyAttribute(lambda a: a.user.first_name)
     last_name = factory.LazyAttribute(lambda a: a.user.last_name)
-    email = factory.LazyAttribute(lambda a: '{0}-{1}@example.com'.format(a.user.first_name, a.user.last_name).lower())
+    email = factory.LazyAttribute(lambda a: u'{0}-{1}@example.com'.format(a.user.first_name, a.user.last_name).lower())
     user = factory.SubFactory(UserFactory)
     role = "Customer"
 
