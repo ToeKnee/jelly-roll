751aae9aeae722dc98f7366154c2401fc66e908e
Anthony Batchelor <tony@ynottony.net>
2015-03-01 19:16:04 +0000

    PEP8 Fixes

--- satchmo/configuration/views.py
+++ satchmo/configuration/views.py
@@ -7,6 +7,7 @@ import logging
 
 log = logging.getLogger(__name__)
 
+
 def group_settings(request, group, template='configuration/group_settings.html'):
     # Determine what set of settings this editor is used for
     mgr = ConfigurationSettings()
@@ -18,9 +19,6 @@ def group_settings(request, group, template='configuration/group_settings.html')
         title = settings.name
         log.debug('title: %s', title)
 
-    # Create an editor customized for the current user
-    #editor = forms.customized_editor(settings)
-
     if request.method == 'POST':
         # Populate the form with user-submitted data
         data = request.POST.copy()
@@ -31,22 +29,21 @@ def group_settings(request, group, template='configuration/group_settings.html')
                 group, key = name.split('__')
                 cfg = mgr.get_config(group, key)
                 if cfg.update(value):
-
                     # Give user feedback as to which settings were changed
                     request.user.message_set.create(message='Updated %s on %s' % (cfg.key, cfg.group.key))
 
             return HttpResponseRedirect(request.path)
     else:
         # Leave the form populated with current setting values
-        #form = editor()
         form = forms.SettingsEditor(settings=settings)
 
     return render_to_response(template, {
         'title': title,
-        'group' : group,
+        'group': group,
         'form': form,
     }, context_instance=RequestContext(request))
 group_settings = staff_member_required(group_settings)
+
 
 # Site-wide setting editor is identical, but without a group
 # staff_member_required is implied, since it calls group_settings
