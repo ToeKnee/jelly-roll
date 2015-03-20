4c1e45ee5562ed45fce125255c13336e7116f25e
Anthony Batchelor <tony@ynottony.net>
2015-03-20 20:10:52 +0000

    Change order_fulfilment command output to logging

--- satchmo/fulfilment/management/commands/order_fulfilment.py
+++ satchmo/fulfilment/management/commands/order_fulfilment.py
@@ -4,6 +4,9 @@ from django.core.management.base import BaseCommand
 from satchmo.configuration import config_value
 from satchmo.shop.models import Order
 
+import logging
+logger = logging.getLogger(__name__)
+
 
 class Command(BaseCommand):
     # args = ''
@@ -11,13 +14,13 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         for fulfilment_house in config_value("FULFILMENT", "MODULES"):
-            self.stdout.write("Fulfilling with {house}".format(house=fulfilment_house))
+            logger.debug("Fulfilling with {house}".format(house=fulfilment_house))
             api_module = "{fulfilment_house}.api".format(
                 fulfilment_house=fulfilment_house,
             )
             module = importlib.import_module(api_module)
             for order in Order.objects.unfulfilled().order_by("time_stamp"):
                 if module.send_order(order):
-                    self.stdout.write(u'Order fulfilment processed "%s"' % order)
+                    logger.info(u'Order fulfilment processed "%s"' % order)
                 else:
-                    self.stdout.write(u'Order fulfilment not processed,  Something went wrong. "%s"' % order)
+                    logger.warning(u'Order fulfilment not processed,  Something went wrong. "%s"' % order)
