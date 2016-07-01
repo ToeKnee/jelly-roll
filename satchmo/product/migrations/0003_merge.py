d77b2b5f99ae6d11bf7855058bd9e6412878a642
Anthony Batchelor <tony@ynottony.net>
2016-07-01 13:45:44 +0100

    Merge migrations

---
+++ satchmo/product/migrations/0003_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('product', '0002_remove_product_ingredients'),
+        ('product', '0002_auto_20150713_1432'),
+    ]
+
+    operations = [
+    ]
