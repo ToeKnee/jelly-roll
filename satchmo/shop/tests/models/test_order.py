cf8a66ade675906c36418944ab2e30b33e180bbf
Anthony Batchelor <tony@ynottony.net>
2016-07-27 19:55:58 +0100

    :blush: renamed method without renaming tested method...

--- satchmo/shop/tests/models/test_order.py
+++ satchmo/shop/tests/models/test_order.py
@@ -24,13 +24,13 @@ class OrderShippingDateTest(TestCase):
     def test_not_frozen__business_day__before_midday(self, now):
         now.return_value = timezone.datetime(2016, 7, 25, 11, 58)  # Monday
         order = OrderFactory()
-        self.assertEqual(order._shipping_date(), datetime.date(2016, 7, 25))
+        self.assertEqual(order.shipping_date(), datetime.date(2016, 7, 25))
 
     @mock.patch("satchmo.shop.models.timezone.now")
     def test_not_frozen__business_day__after_midday(self, now):
         now.return_value = timezone.datetime(2016, 7, 25, 14, 14)  # Monday
         order = OrderFactory()
-        self.assertEqual(order._shipping_date(), datetime.date(2016, 7, 26))
+        self.assertEqual(order.shipping_date(), datetime.date(2016, 7, 26))
 
     @mock.patch("satchmo.shop.models.timezone.now")
     def test_not_frozen__weekend(self, now):
@@ -38,7 +38,7 @@ class OrderShippingDateTest(TestCase):
         # next business day.
         now.return_value = timezone.datetime(2016, 7, 30, 10, 00)  # Saturday
         order = OrderFactory()
-        self.assertEqual(order._shipping_date(), datetime.date(2016, 8, 1))
+        self.assertEqual(order.shipping_date(), datetime.date(2016, 8, 1))
 
     @mock.patch("satchmo.shop.models.timezone.now")
     def test_frozen__shipped(self, now):
@@ -46,22 +46,22 @@ class OrderShippingDateTest(TestCase):
         now.return_value = timezone.datetime(2016, 7, 30, 12, 15)  # Saturday
         order = PaidOrderFactory()
         order.add_status('Shipped', "Test Order Shipped")
-        self.assertEqual(order._shipping_date(), datetime.date(2016, 7, 30))
+        self.assertEqual(order.shipping_date(), datetime.date(2016, 7, 30))
 
     @mock.patch("satchmo.shop.models.timezone.now")
     def test_frozen__business_day__not_shipped__before_midday(self, now):
         now.return_value = timezone.datetime(2016, 7, 25, 11, 58)  # Monday
         order = PaidOrderFactory()
-        self.assertEqual(order._shipping_date(), datetime.date(2016, 7, 25))
+        self.assertEqual(order.shipping_date(), datetime.date(2016, 7, 25))
 
     @mock.patch("satchmo.shop.models.timezone.now")
     def test_frozen__business_day__not_shipped__after_midday(self, now):
         now.return_value = timezone.datetime(2016, 7, 25, 14, 14)  # Monday
         order = PaidOrderFactory()
-        self.assertEqual(order._shipping_date(), datetime.date(2016, 7, 26))
+        self.assertEqual(order.shipping_date(), datetime.date(2016, 7, 26))
 
     @mock.patch("satchmo.shop.models.timezone.now")
     def test_frozen__weekend__not_shipped(self, now):
         now.return_value = timezone.datetime(2016, 7, 30, 15, 15)  # Saturday
         order = PaidOrderFactory()
-        self.assertEqual(order._shipping_date(), datetime.date(2016, 8, 1))
+        self.assertEqual(order.shipping_date(), datetime.date(2016, 8, 1))
