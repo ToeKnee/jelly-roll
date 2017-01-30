5c925f85b36382f8901086d79e2ecd37109d8890
Anthony Batchelor <tony@ynottony.net>
2017-01-30 21:41:26 +0000

    Add Currency model

--- satchmo/currency/admin.py
+++ satchmo/currency/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Currency
+
+
+@admin.register(Currency)
+class CurrencyAdmin(admin.ModelAdmin):
+    filter_horizontal = ("countries", )
+    list_display = ("iso_4217_code", "name", "symbol", "minor_symbol")
---
+++ satchmo/currency/migrations/0002_currency.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('l10n', '0001_initial'),
+        ('currency', '0001_initial'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Currency',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('iso_4217_code', models.CharField(max_length=3, verbose_name='ISO 4217 code')),
+                ('name', models.CharField(max_length=255, verbose_name='Name')),
+                ('symbol', models.CharField(max_length=5, verbose_name='Symbol')),
+                ('minor_symbol', models.CharField(help_text='Pence, Cent, Sen, etc.', max_length=5, verbose_name='Minor Symbol')),
+                ('countries', models.ManyToManyField(related_name='currency', to='l10n.Country')),
+            ],
+            options={
+            },
+            bases=(models.Model,),
+        ),
+    ]
