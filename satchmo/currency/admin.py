124743ad45b027b8597da4684e3ec3cb6f0c9a50
Anthony Batchelor <tony@ynottony.net>
2017-02-06 21:26:34 +0000

    Handle Primary and Accepted currencies

--- satchmo/currency/admin.py
+++ satchmo/currency/admin.py
@@ -6,4 +6,7 @@ from .models import Currency
 @admin.register(Currency)
 class CurrencyAdmin(admin.ModelAdmin):
     filter_horizontal = ("countries", )
-    list_display = ("iso_4217_code", "name", "symbol", "minor_symbol")
+    list_display = ("iso_4217_code", "name", "symbol", "minor_symbol", "primary", "accepted")
+    list_filter = ("accepted", )
+    search_fields = ("iso_4217_code", "name", "symbol", "countries__printable_name")
+    ordering = ("-primary", "-accepted", "iso_4217_code")
--- satchmo/currency/tests.py
+++ satchmo/currency/tests.py
@@ -1,3 +1,32 @@
 from django.test import TestCase
 
-# Create your tests here.
+from .models import Currency
+
+
+class CurrencyTest(TestCase):
+    def test_uncode(self):
+        currency = Currency(iso_4217_code="GBP")
+
+        self.assertEqual(u"{currency}".format(currency=currency), "GBP")
+
+    def test_save__set_primary__sets_accepted(self):
+        currency = Currency.objects.get(iso_4217_code="GBP")
+        currency.primary = True
+        currency.save()
+
+        currency = Currency.objects.get(iso_4217_code="GBP")
+        self.assertTrue(currency.primary)
+        self.assertTrue(currency.accepted)
+
+    def test_save__set_primary__turns_off_other_primary(self):
+        currency = Currency.objects.get(iso_4217_code="GBP")
+        currency.primary = True
+        currency.save()
+
+        # Make Euro primary currency
+        currency = Currency.objects.get(iso_4217_code="EUR")
+        currency.primary = True
+        currency.save()
+
+        currency = Currency.objects.get(iso_4217_code="GBP")
+        self.assertFalse(currency.primary)
