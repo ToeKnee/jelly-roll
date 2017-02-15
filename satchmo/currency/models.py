4d7f7c61af20f981f6c8a27d062e203b47415dc8
Anthony Batchelor <tony@ynottony.net>
2017-02-15 20:43:23 +0000

    Add ExchangeRate model

--- satchmo/currency/models.py
+++ satchmo/currency/models.py
@@ -1,7 +1,20 @@
+import datetime
+
 from django.db import models
 from django.utils.translation import ugettext_lazy as _
 
 from satchmo.l10n.models import Country
+
+
+class CurrencyManager(models.Manager):
+    def get_primary(self):
+        return Currency.objects.get(primary=True)
+
+    def accepted(self):
+        return Currency.objects.filter(primary=False, accepted=True)
+
+    def all_accepted(self):
+        return Currency.objects.filter(accepted=True)
 
 
 class Currency(models.Model):
@@ -13,6 +26,8 @@ class Currency(models.Model):
 
     primary = models.BooleanField(_("Primary"), help_text=_("Primary currency for the shop"), default=False)
     accepted = models.BooleanField(_("Accepted"), help_text=_("Accepted alternative currency for the shop"), default=False)
+
+    objects = CurrencyManager()
 
     class Meta:
         verbose_name = _('Currency')
@@ -29,3 +44,23 @@ class Currency(models.Model):
                 self.accepted = True
 
         return super(Currency, self).save(*args, **kwargs)
+
+
+class ExchangeRate(models.Model):
+    currency = models.ForeignKey(Currency, related_name="exchange_rates", editable=False)
+    date = models.DateField(_("Date"), default=datetime.date.today, editable=False)
+    rate = models.DecimalField(_("Rate"), help_text=_("Rate from primary currency"), max_digits=6, decimal_places=4, editable=False)
+
+    class Meta:
+        verbose_name = _('Exchange Rate')
+        verbose_name_plural = _('Exchange Rates')
+        unique_together = ('currency', 'date')
+        get_latest_by = "date"
+        ordering = ("-date", )
+        order_with_respect_to = 'currency'
+
+    def __unicode__(self):
+        return u"{currency} {rate}".format(
+            currency=self.currency,
+            rate=self.rate
+        )
