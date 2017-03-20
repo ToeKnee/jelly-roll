8da90cb57d0071799dd0a41d719d56c9872b0bd1
Anthony Batchelor <tony@ynottony.net>
2017-03-20 22:10:33 +0000

    Refactor currency tests

--- satchmo/currency/tests/test_exchange_rate_client.py
+++ satchmo/currency/tests/test_exchange_rate_client.py
@@ -7,37 +7,8 @@ from django.core.management import call_command
 from django.test import TestCase
 from django.utils.six import StringIO
 
-from .models import Currency, ExchangeRate
-from .modules.fixer import FixerEchangeRateClient
-
-
-class CurrencyTest(TestCase):
-    def test_uncode(self):
-        currency = Currency(iso_4217_code="GBP")
-
-        self.assertEqual(u"{currency}".format(currency=currency), "GBP")
-
-    def test_save__set_primary__sets_accepted(self):
-        currency = Currency.objects.get(iso_4217_code="GBP")
-        currency.primary = True
-        currency.save()
-
-        currency = Currency.objects.get(iso_4217_code="GBP")
-        self.assertTrue(currency.primary)
-        self.assertTrue(currency.accepted)
-
-    def test_save__set_primary__turns_off_other_primary(self):
-        currency = Currency.objects.get(iso_4217_code="GBP")
-        currency.primary = True
-        currency.save()
-
-        # Make Euro primary currency
-        currency = Currency.objects.get(iso_4217_code="EUR")
-        currency.primary = True
-        currency.save()
-
-        currency = Currency.objects.get(iso_4217_code="GBP")
-        self.assertFalse(currency.primary)
+from satchmo.currency.models import Currency, ExchangeRate
+from satchmo.currency.modules.fixer import FixerEchangeRateClient
 
 
 class FixerExchangeRateClientTest(TestCase):
