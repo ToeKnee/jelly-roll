b1ba0bfd00bd0e8a63cf5ecacfe84a27e69aee09
Anthony Batchelor <tony@ynottony.net>
2017-03-31 16:49:31 +0100

    Add CurrencyFactory

--- satchmo/currency/factories.py
+++ satchmo/currency/factories.py
@@ -1,8 +1,40 @@
+# -*- coding: utf-8 -*-
 import factory
 
 from decimal import Decimal
 
+from satchmo.l10n.models import Country
 from .models import Currency, ExchangeRate
+
+
+class CurrencyFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Currency
+        django_get_or_create = ('iso_4217_code',)
+
+    iso_4217_code = "EUR"
+    name = "Euro"
+    symbol = "€"
+    minor_symbol = "c"
+
+    @factory.post_generation
+    def countries(self, create, extracted, **kwargs):
+        if not create:
+            # Simple build, do nothing.
+            return
+
+            if extracted:
+                # A list of countries were passed in, use them
+                for country in extracted:
+                    self.countries.add(country)
+            else:
+                self.countries = Country.objects.filter(printable_name__in=[
+                    "Austria", "Belgium", "Cyprus", "Estonia",
+                    "Finland", "France", "Germany", "Greece",
+                    "Ireland", "Italy", "Latvia", "Lithuania",
+                    "Luxembourg", "Malta", "Netherlands", "Portugal",
+                    "Slovakia", "Slovenia", "Spain"
+                ])
 
 
 class ExchangeRateFactory(factory.django.DjangoModelFactory):
