d8f287f1d4f5db20ac858edff3493f4cade9cc1d
Anthony Batchelor <tony@ynottony.net>
2017-04-10 11:30:31 +0100

    Update Purchase Order to be multi-currency aware

--- satchmo/payment/modules/purchaseorder/models.py
+++ satchmo/payment/modules/purchaseorder/models.py
@@ -1,22 +1,24 @@
-try:
-    from decimal import Decimal
-except:
-    from django.utils._decimal import Decimal
+import config
+from decimal import Decimal
 
 from django.db import models
 from django.utils.safestring import mark_safe
 from django.utils.translation import ugettext, ugettext_lazy as _
+
 from satchmo.currency.utils import money_format
 from satchmo.shop.models import Order
+
 
 class PurchaseOrder(models.Model):
     po_number = models.CharField(_('Customer PO Number'), max_length=20)
     order = models.ForeignKey(Order)
-    balance = models.DecimalField(_("Outstanding Balance"),
-        max_digits=18, decimal_places=10, blank=True, null=True)
+    balance = models.DecimalField(
+        _("Outstanding Balance"),
+        max_digits=18, decimal_places=10, blank=True, null=True
+    )
     paydate = models.DateField(_('Paid on'), blank=True, null=True)
     notes = models.TextField(_('Notes'), blank=True, null=True)
-    
+
     def __unicode__(self):
         return "PO: #%s" % self.po_number
 
@@ -25,24 +27,19 @@ class PurchaseOrder(models.Model):
             b = self.balance
         else:
             b = Decimal('0.00')
-            
-        return money_format(b)
+
+        return money_format(b, self.order.currency.iso_4217_code)
 
     def order_link(self):
         return mark_safe(u'<a href="/admin/shop/order/%i/">%s #%i (%s)</a>' % (
             self.order.id,
-            ugettext('Order'), 
-            self.order.id, 
-            money_format(self.order.total)))
-            
+            ugettext('Order'),
+            self.order.id,
+            money_format(self.order.total, self.order.currency.iso_4217_code)
+        ))
     order_link.allow_tags = True
-    
+
     def save(self, *args, **kwargs):
         if self.balance is None:
             self.balance = self.order.balance
         super(PurchaseOrder, self).save(*args, **kwargs)
-    
-    
-            
-
-import config
