2cb80475e9f959979e7bfc9428d5fae517f56367
Anthony Batchelor <tony@ynottony.net>
2017-05-03 10:53:38 +0100

    Fix discount utils

--- satchmo/discount/utils.py
+++ satchmo/discount/utils.py
@@ -1,24 +1,23 @@
-try:
-    from decimal import Decimal
-except:
-    from django.utils._decimal import Decimal
-
 import datetime
-import logging
-import types
-from satchmo.discount.models import Discount, NullDiscount
-from satchmo.shop.models import Product
 
+from decimal import Decimal
+
+from satchmo.discount.models import Discount, NullDiscount
+from satchmo.product.models import Product
+
+import logging
 log = logging.getLogger(__name__)
+
 
 def calc_by_percentage(price, percentage):
     if percentage > 1:
         log.warn("Correcting discount percentage, should be less than 1, is %s", percentage)
-        percentage = percentage/100
+        percentage = percentage / 100
 
-    work = price * (1-percentage)
+    work = price * (1 - percentage)
     cents = Decimal("0.01")
     return work.quantize(cents)
+
 
 def find_discount_for_code(code):
     discount = None
@@ -35,12 +34,19 @@ def find_discount_for_code(code):
 
     return discount
 
+
 def find_auto_discounts(products):
-    if products.__class__ == Product:
+    if isinstance(products, Product):
         products = [products]
     today = datetime.date.today()
-    discs = Discount.objects.filter(automatic=True, active=True, startDate__lte=today, endDate__gt=today)
+    discs = Discount.objects.filter(
+        automatic=True,
+        active=True,
+        startDate__lte=today,
+        endDate__gt=today
+    )
     return discs.filter(validProducts__in=products).order_by('-percentage')
+
 
 def find_best_auto_discount(product):
     discs = find_auto_discounts(product)
