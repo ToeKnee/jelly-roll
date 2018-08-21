c33c94aeaceeb9c4145253ef5b5e65f7541198f3
Anthony Batchelor <tony@ynottony.net>
2018-08-21 10:38:52 +0100

    Empty shipping migrations
    Covered by tieredweightzone migrations, split during Django 2.1 upgrade

--- satchmo/shipping/migrations/0004_auto_20160728_2034.py
+++ satchmo/shipping/migrations/0004_auto_20160728_2034.py
@@ -11,34 +11,4 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
-        migrations.AddField(
-            model_name='carrier',
-            name='estimated_delivery_expected_days',
-            field=models.PositiveIntegerField(default=7, verbose_name='Usual number of days after shipping until delivery'),
-            preserve_default=True,
-        ),
-        migrations.AddField(
-            model_name='carrier',
-            name='estimated_delivery_max_days',
-            field=models.PositiveIntegerField(default=25, verbose_name='Maximum number of days after shipping until delivery'),
-            preserve_default=True,
-        ),
-        migrations.AddField(
-            model_name='carrier',
-            name='estimated_delivery_min_days',
-            field=models.PositiveIntegerField(default=1, verbose_name='Minimum number of days after shipping until delivery'),
-            preserve_default=True,
-        ),
-        migrations.AlterField(
-            model_name='carriertranslation',
-            name='languagecode',
-            field=models.CharField(max_length=10, verbose_name='language', choices=[(b'en', b'English')]),
-            preserve_default=True,
-        ),
-        migrations.AlterField(
-            model_name='zonetranslation',
-            name='languagecode',
-            field=models.CharField(max_length=10, verbose_name='language', choices=[(b'en', b'English')]),
-            preserve_default=True,
-        ),
     ]
